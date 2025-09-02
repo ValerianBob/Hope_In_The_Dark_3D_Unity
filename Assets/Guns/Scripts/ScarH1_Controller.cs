55c22d09551e21b245b8018af638c2d3ef0927b6
Valerian <walerian813@gmail.com>
2025-09-02 23:08:50 +0300

    Added reload
    Now have bullets in magazine and in inventory. Can reload

--- Assets/Guns/Scripts/ScarH1_Controller.cs
+++ Assets/Guns/Scripts/ScarH1_Controller.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,33 +7,43 @@ public class ScarH1_Controller : MonoBehaviour
 {
     public Camera Camera;
 
-    public GameObject spawnPosition;
-    public GameObject bullet;
-
     private Ray ray;
     private RaycastHit hit;
-    
+
+    public TextMeshProUGUI bulletsInMagazineText;
+    public TextMeshProUGUI bulletsInInventoryText;
+
     private float fireRate = 0.1f;
 
     private float nextFireTime = 0f;
 
-    //bullets
-    private const int bulletsInMagasine = 20;
-
+    public int maxBulletsInMagazine = 20;
     public int currentBulletsInMagasine = 0;
     public int bulletsInInventory = 90;
+    public int reloadTime = 2;
+
+    private bool isReloading = false;
 
     void Start()
     {
+        bulletsInMagazineText.text = maxBulletsInMagazine.ToString();
+        bulletsInInventoryText.text = bulletsInInventory.ToString();
 
+        currentBulletsInMagasine = maxBulletsInMagazine;
     }
 
     void Update()
     {
-        if (Mouse.current.leftButton.isPressed && Time.time >= nextFireTime)
+        if (Mouse.current.leftButton.isPressed && Time.time >= nextFireTime && !isReloading && currentBulletsInMagasine > 0)
         {
             Shot();
             nextFireTime = Time.time + fireRate;
+        }
+
+        if (Keyboard.current.rKey.wasPressedThisFrame && currentBulletsInMagasine != maxBulletsInMagazine && !isReloading)
+        {
+            StartCoroutine(Reload());
+            
         }
     }
 
@@ -39,19 +51,55 @@ public class ScarH1_Controller : MonoBehaviour
     {
         ray = Camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 
-        if (Physics.Raycast(ray, out hit, 1000f))
+        if (currentBulletsInMagasine != 0)
         {
-            Debug.Log("Hit: " + hit.collider.name + " at " + hit.point);
-            Debug.DrawLine(ray.origin, hit.point, Color.green);
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                Debug.Log("Hit: " + hit.collider.name + " at " + hit.point);
+                Debug.DrawLine(ray.origin, hit.point, Color.green);
 
-            hit.collider.gameObject.GetComponent<TargetController>().Death();
+                if (hit.collider.gameObject.GetComponent<TargetController>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<TargetController>().Death();
+                }
+
+                currentBulletsInMagasine -= 1;
+                bulletsInMagazineText.text = currentBulletsInMagasine.ToString();
+
+                SoundsController.Instance.PlayGunShot(0, transform.position);
+            }
+            else
+            {
+                currentBulletsInMagasine -= 1;
+                bulletsInMagazineText.text = currentBulletsInMagasine.ToString();
+
+                SoundsController.Instance.PlayGunShot(0, transform.position);
+            }
+        }
+    }
+
+    private IEnumerator Reload()
+    {
+        isReloading = true;
+
+        yield return new WaitForSeconds(reloadTime);
+
+        int bulletsNeeded = maxBulletsInMagazine - currentBulletsInMagasine;
+
+        if (bulletsInInventory >= bulletsNeeded)
+        {
+            currentBulletsInMagasine += bulletsNeeded;
+            bulletsInInventory -= bulletsNeeded;
         }
         else
         {
-            Debug.Log("No hit");
-            Debug.DrawRay(ray.origin, ray.direction * 1000f, Color.red);
+            currentBulletsInMagasine += bulletsInInventory;
+            bulletsInInventory = 0;
         }
 
-        SoundsController.Instance.PlayGunShot(0, transform.position);
+        bulletsInMagazineText.text = currentBulletsInMagasine.ToString();
+        bulletsInInventoryText.text = bulletsInInventory.ToString();
+
+        isReloading = false;
     }
 }
