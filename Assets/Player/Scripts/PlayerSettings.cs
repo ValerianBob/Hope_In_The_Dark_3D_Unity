0dca51045da4bc959bf08e99c05763d38ae78255
Valerian <walerian813@gmail.com>
2025-10-06 13:56:14 +0300

    Settings
    Added Settings in GameMenu

---
+++ Assets/Player/Scripts/PlayerSettings.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerSettings : MonoBehaviour
+{
+    [HideInInspector]
+    public static PlayerSettings Instance;
+
+    public float PlayerSensitivity;
+
+    public float SoundVolume;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/UserInterface/Scripts/InGameMenu.cs
+++ Assets/UserInterface/Scripts/InGameMenu.cs
@@ -1,3 +1,5 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -6,10 +8,19 @@ using UnityEngine.UI;
 public class InGameMenu : MonoBehaviour
 {
     public GameObject InGameMenuPanel;
+    public GameObject Settings;
+
+    public TextMeshProUGUI sensitivityNumberText;
+    public TextMeshProUGUI soundNumberText;
+
+    public Slider sensitivitySlider;
+    public Slider soundSlider;
 
     public CharacterMovement cm;
 
     public Button ContinueButton;
+    public Button SettingsButton;
+    public Button BackButton;
     public Button ExitButton;
 
     [HideInInspector]
@@ -17,8 +28,17 @@ public class InGameMenu : MonoBehaviour
 
     void Start()
     {
+        sensitivitySlider.value = 0.2f;
+        soundSlider.value = 1f;
+
+        sensitivityNumberText.text = sensitivitySlider.value.ToString();
+        soundNumberText.text = soundSlider.value.ToString();
+
         ContinueButton.onClick.AddListener(Continue);
         ExitButton.onClick.AddListener(Exit);
+
+        SettingsButton.onClick.AddListener(ShowSettingsMenu);
+        BackButton.onClick.AddListener(BackInGameMenu);
     }
 
     void Update()
@@ -44,6 +64,12 @@ public class InGameMenu : MonoBehaviour
                 Cursor.visible = false;
             }
         }
+
+        PlayerSettings.Instance.PlayerSensitivity = Mathf.Round(sensitivitySlider.value * 10f) / 10f;
+        PlayerSettings.Instance.SoundVolume = Mathf.Round(soundSlider.value * 10f) / 10f; 
+
+        sensitivityNumberText.text = (Mathf.Round(sensitivitySlider.value * 10f) / 10f).ToString();
+        soundNumberText.text = (Mathf.Round(soundSlider.value * 10f) / 10f).ToString(); 
     }
 
     private void Continue()
@@ -54,6 +80,24 @@ public class InGameMenu : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    private void ShowSettingsMenu()
+    {
+        Settings.SetActive(true);
+
+        ContinueButton.gameObject.SetActive(false);
+        SettingsButton.gameObject.SetActive(false);
+        ExitButton.gameObject.SetActive(false);
+    }
+
+    private void BackInGameMenu()
+    {
+        Settings.SetActive(false);
+
+        ContinueButton.gameObject.SetActive(true);
+        SettingsButton.gameObject.SetActive(true);
+        ExitButton.gameObject.SetActive(true);
     }
 
     private void Exit()
